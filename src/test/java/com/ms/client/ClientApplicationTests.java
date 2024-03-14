6ede219142b7df6de23b4edac2bd8016f39bc039
Neuber Paiva de Souza <neuber.paiva@gmail.com>
2024-03-14 10:43:09 -0300

    Update ClientApplicationTests.java

--- src/test/java/com/ms/client/ClientApplicationTests.java
+++ src/test/java/com/ms/client/ClientApplicationTests.java
@@ -1,10 +1,9 @@
 package com.ms.client;
 
-import com.ms.client.controller.ClientControllerTests;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = ClientControllerTests.ClientApplication.class)
+@SpringBootTest(classes = ClientApplication.class)
 public class ClientApplicationTests {
 
 	@Test
